bb4238f01d05618542908385eeb2fc9e4633d162
Emir Peskovic <me@emir.dk>
2022-06-16 20:11:28 +0200

    map change, sensor change, user profile change

--- src/sensor.py
+++ src/sensor.py
@@ -1,11 +1,12 @@
-from database_manager import Base, Column, Integer, String, ForeignKey, Enum, DateTime
+from database_manager import Base, Column, Integer, ForeignKey, Enum, DateTime, Float
 from parking_status import ParkingStatus
 
 
 class Sensor(Base):
     __tablename__ = "sensors"
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
     parking_status = Column(Enum(ParkingStatus))
     updated_at = Column(DateTime)
     user_id = Column(Integer, ForeignKey("users.id"))
